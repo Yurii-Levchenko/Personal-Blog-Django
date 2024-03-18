555eb959b22c1c55f7002315c2cd46fc3e7554f7
Yura Levchenko <levchenko.yurii024@gmail.com>
2024-03-18 14:39:48 +0200

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Author, Post, Tag, Comment
+
+# Register your models here.
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("full_name",)
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ('title', 'date', "author")
+    list_filter = ('author', 'date', 'tags')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('caption',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'post')
+
+# enough to registrate and manage data on admin panel
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment, CommentAdmin)
